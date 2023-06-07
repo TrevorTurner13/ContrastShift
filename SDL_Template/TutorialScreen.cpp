95affd582cca908993a0db464e5b68cfeb2c072a
Brad-The-Chad <bradley.brooker@easternstudent.ca>
2023-06-07 10:57:35 -0300

    Continued working on Tutorial Screen

--- SDL_Template/TutorialScreen.cpp
+++ SDL_Template/TutorialScreen.cpp
@@ -1,7 +1,11 @@
 #include "TutorialScreen.h"
 
 TutorialScreen::TutorialScreen() {
-	
+	mMoveLeft = new GLTexture("Move Left", "Emulogic.ttf", 50, { 230,0,0 });
+	mMoveRight= new GLTexture("Move Right", "Emulogic.ttf", 50, { 230,0,0 });
+	mJump= new GLTexture("Jump", "Emulogic.ttf", 50, { 230,0,0 });
+	mShift= new GLTexture("Shift Contrast", "Emulogic.ttf", 50, { 230,0,0 });
+	mPush= new GLTexture("Push Object", "Emulogic.ttf", 50, { 230,0,0 });
 }
 
 
@@ -15,6 +19,10 @@ void TutorialScreen::Update() {
 }
 
 void TutorialScreen::Render() {
+	mMoveLeft->Render();
+	mMoveRight->Render();
+	mJump->Render();
+	mShift->Render();
+	mPush->Render();
 
-	
 }
